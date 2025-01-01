314ad802dabbe98d831d6135ed994811878f53c6
William Stocks <willstocks@me.com>
2025-01-01 12:43:04 +0000

    Add urlpatterns to games app

---
+++ games/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import GameListCreateView, GameDetailView, LikeCreateView, CommentListCreateView
+
+urlpatterns = [
+    path('games/', GameListCreateView.as_view(), name='game-list-create'),
+    path('games/<int:pk>/', GameDetailView.as_view(), name='game-detail'),
+    path('games/<int:pk>/like/', LikeCreateView.as_view(), name='game-like'),
+    path('games/<int:pk>/comments/', CommentListCreateView.as_view(), name='game-comments'),
+]
