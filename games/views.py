11ec034696c3328c5d52ff268549976a274a1c40
William Stocks <willstocks@me.com>
2025-01-01 12:40:01 +0000

    Add classes to views in games app

--- games/views.py
+++ games/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from .models import Game, Like, Comment
+from .serializers import GameSerializer, LikeSerializer, CommentSerializer
 
-# Create your views here.
+class GameListCreateView(generics.ListCreateAPIView):
+    queryset = Game.objects.all()
+    serializer_class = GameSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(creator=self.request.user)
+
+class GameDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Game.objects.all()
+    serializer_class = GameSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return Game.objects.filter(creator=self.request.user)
+
+class LikeCreateView(generics.CreateAPIView):
+    queryset = Like.objects.all()
+    serializer_class = LikeSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+class CommentListCreateView(generics.ListCreateAPIView):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
