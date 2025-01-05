068b0c80f8a864e5391fe470e1828adf17b799a0
William Stocks <willstocks@me.com>
2025-01-05 16:19:39 +0000

    Fix Profile issue

--- users/signals.py
+++ users/signals.py
@@ -8,3 +8,12 @@ def create_or_update_user_profile(sender, instance, created, **kwargs):
     if created:
         UserProfile.objects.create(user=instance)
     instance.profile.save()
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
