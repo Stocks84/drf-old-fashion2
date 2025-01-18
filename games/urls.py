7a32873a25f3db4d883d0968f4cd875ab3e25409
William Stocks <willstocks@me.com>
2025-01-18 22:29:48 +0000

    Fix issue with endpoints

--- games/urls.py
+++ games/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from .views import GameListCreateView, GameDetailView, LikeCreateView, CommentListCreateView, GameRecentView
 
 urlpatterns = [
-    path('games/', GameListCreateView.as_view(), name='game-list-create'),
-    path('games/<int:pk>/', GameDetailView.as_view(), name='game-detail'),
-    path('games/<int:pk>/like/', LikeCreateView.as_view(), name='game-like'),
-    path('games/<int:pk>/comments/', CommentListCreateView.as_view(), name='game-comments'),
-    path('games/recent/', GameRecentView.as_view(), name='game-recent'),
-    path('games/<int:game_id>/comments/', CommentListCreateView.as_view(), name='game-comments'),
+    path('', GameListCreateView.as_view(), name='game-list-create'),
+    path('<int:pk>/', GameDetailView.as_view(), name='game-detail'),
+    path('<int:pk>/like/', LikeCreateView.as_view(), name='game-like'),
+    path('<int:pk>/comments/', CommentListCreateView.as_view(), name='game-comments'),
+    path('recent/', GameRecentView.as_view(), name='game-recent'),
+    path('<int:game_id>/comments/', CommentListCreateView.as_view(), name='game-comments'),
 ]
