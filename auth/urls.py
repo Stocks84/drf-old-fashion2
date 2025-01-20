4a4f3ff7bb0e7302c6f5a4d5c37f8fb8cfdc7a2f
William Stocks <willstocks@me.com>
2025-01-20 09:07:07 +0000

    Fixed some urls patterns

--- auth/urls.py
+++ auth/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),  # Login
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),  # Refresh token
     path('api/logout/', LogoutView.as_view(), name='logout'),
-     path('api/games/', include('games.urls')),
+    path('api/games/', include('games.urls')),
 ]
--- users/urls.py
+++ users/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import UserProfileListCreateView, UserProfileDetailView, UserDetailView
 
 urlpatterns = [
-    path('profiles/', UserProfileListCreateView.as_view(), name='profile-list-create'),
-    path('profiles/<int:pk>/', UserProfileDetailView.as_view(), name='profile-detail'),
+    path('', UserProfileListCreateView.as_view(), name='profile-list-create'),
+    path('<int:pk>/', UserProfileDetailView.as_view(), name='profile-detail'),
     path('<int:user_id>/', UserDetailView.as_view(), name='user-detail'),  # **NEW**
 ]
