c6db36fb914fc465ee19b127965ca795357fb6b9
William Stocks <willstocks@me.com>
2025-01-21 10:21:06 +0000

    Fixed the recent Games endpoint issue

--- games/admin.py
+++ games/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Game
 
-# Register your models here.
+@admin.register(Game)
+class GameAdmin(admin.ModelAdmin):
+    list_display = ('title', 'creator', 'created_at', 'updated_at')  # Replace 'name' with 'title'
+    search_fields = ('title', 'description')  # Allow searching by title and description
+    list_filter = ('created_at', 'updated_at')  # Add filters for created and updated times
+
