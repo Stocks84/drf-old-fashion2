3da0e4e65be3363fc7e0d93073db302edbb3783f
William Stocks <willstocks@me.com>
2025-01-24 08:31:01 +0000

    Profile issue fix

--- auth/urls.py
+++ auth/urls.py
@@ -1,6 +1,6 @@
 
 from django.urls import path, include
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView,
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from .views import LogoutView, SignupView, CustomTokenObtainPairView
 
 urlpatterns = [
