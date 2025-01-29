af3663eadcc071cdb50c55f73cbb91cfa0387af2
William Stocks <willstocks@me.com>
2025-01-29 15:27:22 +0000

    Update profile endpoint

--- users/urls.py
+++ users/urls.py
@@ -2,9 +2,8 @@ from django.urls import path
 from .views import UserProfileListCreateView, UserProfileDetailView, UserDetailView
 
 urlpatterns = [
-    path('', UserProfileListCreateView.as_view(), name='profile-list-create'),
-    path('<int:pk>/', UserProfileDetailView.as_view(), name='profile-detail'),
-    path('<int:user_id>/', UserDetailView.as_view(), name='user-detail'),
     path('profiles/', UserProfileListCreateView.as_view(), name='profile-list-create'), 
-    path('profiles/<int:pk>/', UserProfileDetailView.as_view(), name='profile-detail'),
+    path('profiles/<int:pk>/', UserProfileDetailView.as_view(), name='profile-detail'),  
+    path('<int:user_id>/', UserDetailView.as_view(), name='user-detail'),   
+    
 ]
